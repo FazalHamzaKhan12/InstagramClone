6f74eac641dcd6368e174409139847ec0358604a
Fazal Hamza Khan <theunknown.pak@gmail.com>
2024-12-01 09:03:26 +0500

    Profile Screen
    Adding Tab Option

--- lib/Screen/MianPage/ProfileScr/profilescreen.dart
+++ lib/Screen/MianPage/ProfileScr/profilescreen.dart
@@ -7,260 +7,309 @@ class Profilescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0XFF121212),
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         backgroundColor: Color(0XFF121212),
-        centerTitle: true,
-        leading: Icon(
-          CupertinoIcons.news,
-          size: 1,
+        appBar: AppBar(
+          backgroundColor: Color(0XFF121212),
+          centerTitle: true,
+          leading: Icon(
+            CupertinoIcons.news,
+            size: 1,
+          ),
+          title: Row(
+            children: [
+              SizedBox(
+                width: 67,
+              ),
+              Icon(
+                CupertinoIcons.padlock_solid,
+                size: 18,
+                color: Colors.blue,
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              Text(
+                "Fazal_Hamza",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Icon(
+                CupertinoIcons.line_horizontal_3,
+                size: 26,
+              ),
+            )
+          ],
         ),
-        title: Row(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                CircleAvatar(
+                  radius: 55,
+                  child: UiHelper.customImages(imgurl: 'myprofile.png'),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "20",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 22),
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Text(
+                          "752",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 22),
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Text(
+                          "162",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 22),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          "Post",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300, fontSize: 16),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          "Followers",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300, fontSize: 16),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          "Following",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w300, fontSize: 16),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
             SizedBox(
-              width: 67,
+              height: 20,
             ),
-            Icon(
-              CupertinoIcons.padlock_solid,
-              size: 18,
-              color: Colors.blue,
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'Fazal_Hamza',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'Youtube 👇👇👇',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'FazalTechWorld',
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                ),
+              ],
             ),
             SizedBox(
-              width: 2,
+              height: 20,
             ),
-            Text(
-              "Fazal_Hamza",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            Center(
+              child: Container(
+                height: 40,
+                width: 350,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey, // Border color
+                    width: 1.0,
+                  ),
+                  color: Colors.black, // Move color inside BoxDecoration
+                  borderRadius: BorderRadius.circular(
+                      10), // Correct usage of borderRadius
+                ),
+                child: Center(
+                  child: Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14), // Optional text styling
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 25,
+                ),
+                Container(
+                  width:
+                      80, // Diameter of the CircleAvatar (2 * radius + border width)
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.yellow,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: CircleAvatar(
+                    backgroundColor: Color(0XFF121212),
+                    child: Icon(CupertinoIcons.plus),
+                  ),
+                ),
+                SizedBox(
+                  width: 25,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(width: 1, color: Colors.pinkAccent),
+                  ),
+                  child: CircleAvatar(
+                    child: ClipOval(
+                      child: Image.asset(
+                        "assets/images/Myf.jpg", // Or use NetworkImage if the image is from the web.
+                        fit: BoxFit
+                            .cover, // Ensures the image covers the circle without stretching.
+                        width: 80, // Double the radius for a perfect fit.
+                        height: 80, // Double the radius for a perfect fit.
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 25,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.green,
+                        width: 1,
+                      )),
+                  child: CircleAvatar(
+                    child: ClipOval(
+                      child: Image.asset(
+                        "assets/images/1000025770.jpg", // Or use NetworkImage if the image is from the web.
+                        fit: BoxFit
+                            .cover, // Ensures the image covers the circle without stretching.
+                        width: 80, // Double the radius for a perfect fit.
+                        height: 80, // Double the radius for a perfect fit.
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 46,
+                ),
+                Text(
+                  "New",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  width: 76,
+                ),
+                Text(
+                  "Pic",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(
+                  width: 58,
+                ),
+                Text(
+                  "Chappal",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 50,
+              child: AppBar(
+                bottom: TabBar(
+                  indicatorColor: Colors.yellow,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  tabs: [
+                    Tab(
+                      icon: UiHelper.customImages(
+                          imgurl:
+                              "Grid Icon.png"), // Correct usage of the `icon` parameter
+                    ),
+                    Tab(
+                      icon: UiHelper.customImages(
+                          imgurl: "Tags Icon.png"), // Also use `icon` here
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Icon(
-              CupertinoIcons.line_horizontal_3,
-              size: 26,
-            ),
-          )
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              CircleAvatar(
-                radius: 55,
-                child: UiHelper.customImages(imgurl: 'myprofile.png'),
-              ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "20",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 22),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "752",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 22),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "162",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 22),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text(
-                        "Post",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300, fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text(
-                        "Followers",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300, fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text(
-                        "Following",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300, fontSize: 16),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                'Fazal_Hamza',
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                'Youtube 👇👇👇',
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                'FazalTechWorld',
-                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: Container(
-              height: 40,
-              width: 350,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey, // Border color
-                  width: 1.0,
-                ),
-                color: Colors.black, // Move color inside BoxDecoration
-                borderRadius:
-                    BorderRadius.circular(10), // Correct usage of borderRadius
-              ),
-              child: Center(
-                child: Text(
-                  'Edit Profile',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14), // Optional text styling
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 25,),
-          Row(
-            children: [
-              SizedBox(width: 25,),
-              Container(
-                width: 80, // Diameter of the CircleAvatar (2 * radius + border width)
-                height: 80,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.yellow,
-                    width: 1.0,
-                  ),
-                ),
-                child: CircleAvatar(
-                    backgroundColor: Color(0XFF121212),
-                  child: Icon(CupertinoIcons.plus),
-                ),
-              ),
-              SizedBox(width: 25,),
-              Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.pinkAccent
-                  ),
-                ),
-                child: CircleAvatar(
-                  child: ClipOval(
-                    child: Image.asset(
-                      "assets/images/Myf.jpg", // Or use NetworkImage if the image is from the web.
-                      fit: BoxFit
-                          .cover, // Ensures the image covers the circle without stretching.
-                      width: 80, // Double the radius for a perfect fit.
-                      height: 80, // Double the radius for a perfect fit.
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(width: 25,),
-              Container(
-                width: 80, height: 80,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.green,
-                    width: 1,
-                  )
-                ),
-                child: CircleAvatar(
-                  child: ClipOval(
-                    child: Image.asset(
-                      "assets/images/1000025770.jpg", // Or use NetworkImage if the image is from the web.
-                      fit: BoxFit
-                          .cover, // Ensures the image covers the circle without stretching.
-                      width: 80, // Double the radius for a perfect fit.
-                      height: 80, // Double the radius for a perfect fit.
-                    ),
-                  ),
-                ),
-              ),
-              ],
-          ),
-          SizedBox(height: 5,),
-          Row(
-            children: [
-              SizedBox(width: 46,),
-              Text("New",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),),
-              SizedBox(width: 76,),
-              Text("Pic",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),),
-              SizedBox(width: 58,),
-              Text("Chappal",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),),
-            ],
-          ),
-        ],
       ),
     );
   }
