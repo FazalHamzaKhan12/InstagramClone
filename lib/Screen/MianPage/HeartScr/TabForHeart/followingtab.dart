70fb8cd94130cdcba47a730baa9de4be776381c3
Fazal Hamza Khan <theunknown.pak@gmail.com>
2024-12-01 15:11:22 +0500

    Heart Page

---
+++ lib/Screen/MianPage/HeartScr/TabForHeart/followingtab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Followingtab extends StatelessWidget {
+  const Followingtab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.brown,);
+  }
+}
--- lib/Screen/MianPage/HeartScr/heartscreen.dart
+++ lib/Screen/MianPage/HeartScr/heartscreen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
-
+import 'package:instagram_clone/Screen/MianPage/HeartScr/TabForHeart/followingtab.dart';
+import 'package:instagram_clone/Screen/MianPage/HeartScr/TabForHeart/youtab.dart';
 
 class HeartPage extends StatelessWidget {
   const HeartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("??????????"),),);
-  }}
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false, // This ensures no back button is added
+          bottom: TabBar(
+            indicatorColor: Colors.yellow,
+            indicatorSize: TabBarIndicatorSize.tab,
+            unselectedLabelColor: Colors.grey,
+            labelColor: Colors.white,
+            tabs: const [
+              Tab(
+                text: "Following",
+              ),
+              Tab(
+                text: "You",
+              )
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+           Followingtab(),
+            YouTab(),
+          ],
+        ),
+      ),
+    );
+  }
+}
